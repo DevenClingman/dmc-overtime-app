0c5dffbcc6428e6b1f25f997f95e91d9bc7bf7f1
Deven Clingman <dclingy@gmail.com>
2017-08-24 15:36:47 -0600

    brought in live data for admin review page

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,7 +28,8 @@ end
 puts "1 Admin now exists"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user: User.first, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?", 
+    user: User.first, overtime_request: 2.5)
 end
 
 puts "100 posts created"
