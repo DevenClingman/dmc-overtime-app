cfaf9ad2ae31c719ff81243fa5b81887df38bc51
Deven Clingman <dclingy@gmail.com>
2017-08-29 17:17:11 -0600

    updated migration files

--- db/migrate/20170814162050_devise_create_users.rb
+++ db/migrate/20170814162050_devise_create_users.rb
@@ -32,6 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.1]
 
       t.string :first_name
       t.string :last_name
+      t.string :string
       t.string :type
 
       t.timestamps null: false
